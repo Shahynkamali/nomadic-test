7ad1854e6e63a3726bd580919e25e946a1d5f970
shahynkamali <shahynkamali@gmail.com>
2022-03-28 18:36:04 -0500

    added tests

--- src/components/__tests__/ProgramTile.spec.ts
+++ src/components/__tests__/ProgramTile.spec.ts
@@ -1,13 +1,32 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 
 import { mount } from "@vue/test-utils";
 import ProgramTile from "../ProgramTile.vue";
 
 describe("ProgramTile", () => {
+  let enrolled: boolean;
+  beforeEach(() => {
+    enrolled = false;
+  });
   it("renders properly", () => {
     const wrapper = mount(ProgramTile, {
-      props: { program: { title: "Example Program" } },
+      props: { program: { title: "Example Program", id: 1, image: "kljads", enrolled } },
     });
     expect(wrapper.text()).toContain("Example Program");
+  });
+
+  it("renders Start Now is the user is already enrolled", () => {
+    const wrapper = mount(ProgramTile, {
+      props: { program: { title: "Example Program", id: 1, image: "kljads", enrolled } },
+    });
+    expect(wrapper.text()).toContain("Start Now");
+  });
+
+  it("renders Enrolled is the user is already enrolled", () => {
+    enrolled = true;
+    const wrapper = mount(ProgramTile, {
+      props: { program: { title: "Example Program", id: 1, image: "kljads", enrolled } },
+    });
+    expect(wrapper.text()).toContain("Enrolled");
   });
 });
